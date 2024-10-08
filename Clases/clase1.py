eb1efe81b08ac2c1348c925886d1c747cb24957e
Steven Mendoza <smenthor2005@gmail.com>
2024-10-07 21:38:17 -0600

    Clase 1 - 07-10-24

---
+++ Clases/clase1.py
@@ -0,0 +1,54 @@
+## Clase 1:
+
+# Big O
+# Algoritmos de Ordenamiento
+# Algoritmos de Busqueda
+
+# Programacion Funcional
+
+'''
+Tipos de Datos (Datos primitivos) {
+    int
+    float
+    str
+    bool
+}
+'''
+
+entero = 1 # int
+decimal = 3.4 # float
+string = "sfha" # str
+booleano = True # bool (True / False)
+
+a = 0
+c = 1
+d = 0
+b = True and False
+
+if(a==0):
+    print('uwu')
+elif(c==1):
+    print('xdd')
+elif(d==0):
+    print("aña")
+else:
+    print("JIJIJIJA")
+
+
+# a = [a**2 for a in range(1,10)]
+
+while(a<6):
+    print("Todavia en el bucle")
+    a+=1
+
+a = 1
+a = 2.3
+a = "FSSFSF"
+a = True
+a = [1,2,34]
+a = 'Hola Mundo' 
+
+print(a)
+
+
+
