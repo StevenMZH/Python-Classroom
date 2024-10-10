76d32c95fdcb38579e23f6a11550c0634d2cd857
Steven Mendoza <smenthor2005@gmail.com>
2024-10-09 21:01:46 -0600

    Clase 2 - 09/10/24

--- Clases/clase1.py
+++ Clases/clase1.py
@@ -15,41 +15,91 @@ Tipos de Datos (Datos primitivos) {
 }
 '''
 
-entero = 1 # int
-decimal = 3.4 # float
-string = "sfha" # str
-booleano = True # bool (True / False)
+# entero = 10000 # int
+# decimal = 3.4 # float
+# string = "sft7yfguydftha" # str
+# booleano = True # bool (True / False)
 
-a = 0
-c = 1
-d = 0
-b = True and False
+# print(entero + decimal)
 
-if(a==0):
-    print('uwu')
-elif(c==1):
-    print('xdd')
-elif(d==0):
-    print("aña")
-else:
-    print("JIJIJIJA")
+# a = 0
+# c = 1
+# d = 0
+# b = True and False
+
+# print(a)
+
+# # elif = else if
+
+# if(a==0):
+#     print('uwu')
+# elif(c==1):
+#     print('xdd')
+# if(d==0):
+#     print("aña")
+# else:
+#     print("JIJIJIJA")
 
 
 # a = [a**2 for a in range(1,10)]
+# print(a)
 
-while(a<6):
-    print("Todavia en el bucle")
-    a+=1
+# # while(a<6):
+# #     print("Todavia en el bucle")
+# #     a+=1
 
-a = 1
-a = 2.3
-a = "FSSFSF"
-a = True
-a = [1,2,34]
-a = 'Hola Mundo' 
+# a = 1
 
-print(a)
+# a = 1
+# a = 2.3
+# a = "FSSFSF"
+# a = True
+# a = [1,2,34]
+# a = 'Hola Mundo' 
 
-dato = int( input("Dame un int: "))
-print( dato+3 )
+# print(a)
+
+# dato = str( input("Dame un int: ")) 
+# print( dato*3 )
+
+# a = 0 ; b = 1 ; c = a+b ; print(c)
+
+# for i in range(0,101,10):
+#     print(i)
+
+A = [1, 34.5, 'Hola', 'Mundo', '!!', True, [1,2,3]]
+# A.append(13)
+
+# for i in A:
+#     print(i)
+
+
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print('For de Afuera este en la iteracion',i)
+#         print('For de Adentro este en la iteracion',j)
+#         print('________')
+
+
+# for i in range(1,6):
+#     a = int(input('Dame un numero: '))
+#     if(a==0):
+#         continue
+
+#     print(a**2)
+
+# print('Fin')
+
+B = [1,2,3,4,5,6,7,8,9,10]
+C = ['a','b','c','d','e','f','g','h','i','j']
+longitud = len(B)
+
+
+a = 1 in B
+
+B = C
+
+B.clear()
+
+print(C)
 
